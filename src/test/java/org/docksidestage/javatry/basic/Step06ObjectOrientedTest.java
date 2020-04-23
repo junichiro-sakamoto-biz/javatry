3ca85c763dff2967f8f8b60e7faf2599e6e9e55b
sakakendo <sakakendo0321@gmail.com>
2020-04-23 13:02:34 +0900

    change for stage05

--- src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step06ObjectOrientedTest.java
@@ -15,6 +15,7 @@
  */
 package org.docksidestage.javatry.basic;
 
+import org.docksidestage.bizfw.basic.buyticket.OnedayTicket;
 import org.docksidestage.bizfw.basic.buyticket.Ticket;
 import org.docksidestage.bizfw.basic.buyticket.TicketBooth;
 import org.docksidestage.bizfw.basic.objanimal.Animal;
@@ -133,7 +134,8 @@ public class Step06ObjectOrientedTest extends PlainTestCase {
         // _/_/_/_/_/_/_/_/_/_/
         //Ticket ticket = booth.buyOneDayPassport(10000);
         booth.buyOneDayPassport(10000); // as temporary, remove if you finished steo05
-        Ticket ticket = new Ticket(7400); // also here
+//        Ticket ticket = new Ticket(7400); // also here
+        Ticket ticket = new OnedayTicket(74200);
 
         // *buyOneDayPassport() has this process:
         //if (quantity <= 0) {
