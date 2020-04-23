8674252c6636fc1fb13e3578e2979f76c83eee00
sakakendo <sakakendo0321@gmail.com>
2020-04-23 11:26:47 +0900

    finish test_class_moreFix_type

--- src/test/java/org/docksidestage/javatry/basic/Step05ClassTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step05ClassTest.java
@@ -164,6 +164,11 @@ public class Step05ClassTest extends PlainTestCase {
      */
     public void test_class_moreFix_type() {
         // your confirmation code here
+        TicketBooth booth = new TicketBooth();
+        int handedMoney = 20000;
+        TicketBuyResult twoDayPassportResult = booth.buyTwoDayPassport(handedMoney);
+        Ticket twoDayPassport = twoDayPassportResult.getTicket();
+        log(twoDayPassport.getTicketType());
     }
 
     // ===================================================================================
